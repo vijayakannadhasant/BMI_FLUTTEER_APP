963f610f638be8c38e9a06654032dba66e0fafa4
vijayakannadhasant <158731933+vijayakannadhasant@users.noreply.github.com>
2025-07-14 14:28:36 +0530

    Add files via upload

---
+++ android/build.gradle.kts
@@ -0,0 +1,28 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+plugins {
+    // ... other plugins
+
+    // Firebase plugin
+    id("com.google.gms.google-services") version "4.4.3" apply false
+}
+classpath 'com.google.gms:google-services:4.3.15'
